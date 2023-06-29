6002141128cd3d5dfa46db81ad287834e438e419
msaeva <mariq2121@gmail.com>
2023-06-29 10:23:03 +0300

    first commit

---
+++ src/main/java/com/trading212/weathertrip/WeatherTripApplication.java
@@ -0,0 +1,13 @@
+package com.trading212.weathertrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherTripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trading212/weathertrip/WeatherTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trading212.weathertrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
